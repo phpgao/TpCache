20346d3cf697edfe850444debf32890f277be2be
endoffight <endoffight@qq.com>
2015-05-26 19:52:31 +0800

    update to v0.3

--- Plugin.php
+++ Plugin.php
@@ -6,7 +6,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
  *
  * @package TpCache
  * @author 老高
- * @version 0.2
+ * @version 0.3
  * @link http://www.phpgao.com
  */
 class TpCache_Plugin implements Typecho_Plugin_Interface
